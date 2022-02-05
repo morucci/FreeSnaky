5f3c69dbd10998e21b01ea8a6dc7e631c359a163
Fabien Boucher <fabien.dot.boucher@gmail.com>
2022-02-05 06:51:50 +0000

    Partial rework of Tui to comm with server

--- src/Tui.hs
+++ src/Tui.hs
@@ -4,34 +4,36 @@ import Brick
 import Brick.BChan
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.Border.Style as BS
-import Control.Concurrent (forkIO, threadDelay)
+-- import Control.Concurrent (forkIO, threadDelay)
+
+import Control.Concurrent.Async (withAsync)
+import Data.Aeson (encode)
 import qualified Graphics.Vty as V
+import Network.Socket (withSocketsDo)
+import qualified Network.WebSockets as WS
 import Relude
+import qualified Server as S
+  ( ProtoMessages (Bye, Hello, Tick, Welcome),
+    getProtoMessage,
+  )
 import Snake
-  ( AppMem,
-    Direction (..),
+  ( Direction (..),
     Item (..),
-    WStatus (..),
     World (..),
-    getWorld,
-    initAppMem,
-    resetAppMem,
-    runStep,
-    setDirection,
   )
 
 newtype Tick = Tick World
 
 data Name = MainView deriving (Eq, Ord, Show)
 
-data AppState = AppState
-  { appWState :: World,
-    -- TODO: appMem must not be required after client/server segmentation
-    appMem :: AppMem
-  }
+newtype AppState = AppState {appWState :: Maybe World}
 
 drawUI :: AppState -> [Widget Name]
-drawUI s = [withBorderStyle BS.unicodeBold $ B.borderWithLabel (str "Free Snaky") $ vBox rows]
+drawUI (AppState Nothing) = [vBox [str "Waiting for server map"]]
+drawUI (AppState (Just World {..})) =
+  [ withBorderStyle BS.unicodeBold $
+      B.borderWithLabel (str "Free Snaky") $ vBox rows
+  ]
   where
     rows = [hBox $ cellsInRow r | r <- [0 .. height -1]]
     cellsInRow y = [drawCoord (x, y) | x <- [0 .. width -1]]
@@ -44,13 +46,13 @@ drawUI s = [withBorderStyle BS.unicodeBold $ B.borderWithLabel (str "Free Snaky"
         Just COLLISION -> str "X"
         Nothing -> str " Out of bounds"
       Nothing -> error "Out of bounds"
-    height = wHeight $ appWState s
-    width = wWidth $ appWState s
-    m = wFlattenedMap $ appWState s
+    height = wHeight
+    width = wWidth
+    m = wFlattenedMap
 
 handleEvent :: AppState -> BrickEvent Name Tick -> EventM Name (Next AppState)
 handleEvent s (VtyEvent (V.EvKey V.KEsc [])) = halt s
-handleEvent s (AppEvent (Tick newWorld)) = continue $ s {appWState = newWorld}
+handleEvent s (AppEvent (Tick newWorld)) = continue $ s {appWState = Just newWorld}
 handleEvent s (VtyEvent (V.EvKey V.KRight [])) = handleDirEvent s RIGHT
 handleEvent s (VtyEvent (V.EvKey V.KLeft [])) = handleDirEvent s LEFT
 handleEvent s (VtyEvent (V.EvKey V.KUp [])) = handleDirEvent s UP
@@ -59,7 +61,7 @@ handleEvent s _ = continue s
 
 handleDirEvent :: AppState -> Snake.Direction -> EventM Name (Next AppState)
 handleDirEvent s dir = do
-  liftIO $ setDirection (appMem s) dir
+  -- liftIO $ setDirection (appMem s) dir
   continue s
 
 theMap :: AttrMap
@@ -76,18 +78,30 @@ app =
     }
 
 main :: IO ()
-main = do
-  chan <- newBChan 10
-  mem <- initAppMem
-  initialWorld <- getWorld mem
-  let initialState = AppState initialWorld mem
-      buildVty = V.mkVty V.defaultConfig
-      initialSpeed = 500000
-  initialVty <- buildVty
-  void . forkIO . forever $ do
-    runStep mem
-    world <- getWorld mem
-    writeBChan chan $ Tick world
-    threadDelay $ truncate $ initialSpeed / wSpeedFactor world
-    when (wStatus world == GAMEOVER) $ resetAppMem mem
-  void $ customMain initialVty buildVty (Just chan) app initialState
+main = withSocketsDo $ WS.runClient "127.0.0.1" 9160 "/" runApp
+  where
+    runApp :: WS.ClientApp ()
+    runApp conn = do
+      let clientId = "fakeid"
+      WS.sendTextData conn $ encode (S.Hello clientId)
+      resp <- S.getProtoMessage conn
+      case resp of
+        S.Welcome -> do
+          chan <- newBChan 10
+          let initialState = AppState Nothing
+              buildVty = V.mkVty V.defaultConfig
+          initialVty <- buildVty
+          withAsync (readServerMessages chan) $ \_ -> do
+            void $ customMain initialVty buildVty (Just chan) app initialState
+        S.Bye -> do
+          pure ()
+        _ -> print ("Not Implemented" :: Text)
+      where
+        readServerMessages :: BChan Tick -> IO ()
+        readServerMessages chan = do
+          resp <- S.getProtoMessage conn
+          case resp of
+            S.Tick world -> do
+              writeBChan chan $ Tick world
+            _ -> pure ()
+          readServerMessages chan
